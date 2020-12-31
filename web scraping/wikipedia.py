6be5711256048f84c2d9c99815355e241f1a0a15
Daniel M. Alves <dma.fdr@gmail.com>
2020-12-31 19:35:29 -0300

    Add files via upload

---
+++ web scraping/wikipedia.py
@@ -0,0 +1,20 @@
+#!python3
+
+from bs4 import BeautifulSoup as bs
+import requests as rq
+
+while True:
+
+    busca = input("Digite o tema: ")
+    busca = busca.replace(" ", '_')
+    url = "https://pt.wikipedia.org/wiki/"
+    page = rq.get(url=url)
+    soup = bs(page.content, 'html.parser')
+    conteudo = soup.find(class_="mw-parser-output")
+    texto = conteudo.get_text()
+
+    print(texto)
+
+    with open(busca+'.txt', 'wb') as f:
+        f.write(texto.encode())
+    f.close()
