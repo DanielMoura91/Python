c1b97ca3ba006659776474f9ad98a108d55e3dbf
Daniel M. Alves <dma.fdr@gmail.com>
2020-12-31 20:11:49 -0300

    Update variaveis.py

--- tipos/variaveis.py
+++ tipos/variaveis.py
@@ -15,14 +15,22 @@ dayofweek = ['domingo', 'segunda', 'terça',
              'quarta', 'quinta', 'sexta', 'sábado']
 print(f'{dayofweek}')
 
+"""
+# Fibonacci series:
+# the sum of two elements defines the next
+a, b = 0, 1
+while a < 10:
+     print(a, end=',')
+     a, b = b, a+b
+"""
 
 a, b = 0, 1
 
 fibonacci = []
 
-while a < 100:
-    print(a, end=',')
-    a, b = b, a+b
+while a < 100:    
     fibonacci.append(a)
+    a = b 
+    b = a+b    
 
 print('A sequencia de fibonacci é: ', fibonacci)
