29d425781b74a967577761c3f08c476025e25029
Daniel M. Alves <dma.fdr@gmail.com>
2020-12-31 19:07:20 -0300

    Update repeticao.py

--- controle/repeticao.py
+++ controle/repeticao.py
@@ -1,12 +1,15 @@
 #!python3
 
+# imprime valores de 0 a 9
 for i in range(10):
     print(i)
 
+# imprime valores de 1 a 10
 for i in range(1, 11):
     print(i)
 
-for i in range(2, 100, 2):
+# imprime valores de 2 a 100, pulando de 2 em 2
+for i in range(2, 101, 2):
     print(i)
 
 for i in range(99, 1, -2):
