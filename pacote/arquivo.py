b9e05e28881ef1182de1e3d42391829079900190
Daniel M. Alves <dma.fdr@gmail.com>
2021-01-09 01:55:46 -0300

    Update and rename arquivo.py to teste.py

--- pacote/arquivo.py
+++
@@ -1,9 +0,0 @@
-#!python3
-
-idade = 29
-peso = 59.8
-altura = 1.77
-nome = "Daniel"
-boleano = True
-
-print("Meu nome é "+nome+" e peso ", peso, "kg")
---
+++ pacote/teste.py
@@ -0,0 +1,10 @@
+#!python3
+
+idade = 29
+peso = 59.8
+altura = 1.77
+nome = "Daniel"
+boleano = True
+imc = peso / (altura**2)
+
+print(f'Meu nome é {nome}, tenho {idade} anos e meu imc é de {imc}')
