3b567060657676ee8b2fb74894bbf73cb9889d56
Daniel M. Alves <dma.fdr@gmail.com>
2021-01-09 01:59:35 -0300

    Update wikipedia.py
    Correção da variável busca na pesquisa

--- web scraping/wikipedia.py
+++ web scraping/wikipedia.py
@@ -7,7 +7,7 @@ while True:
 
     busca = input("Digite o tema: ")
     busca = busca.replace(" ", '_')
-    url = "https://pt.wikipedia.org/wiki/"
+    url = "https://pt.wikipedia.org/wiki/"+busca
     page = rq.get(url=url)
     soup = bs(page.content, 'html.parser')
     conteudo = soup.find(class_="mw-parser-output")
