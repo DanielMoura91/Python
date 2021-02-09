9a05c31c9061992d92bfb419297c19975db5d0ed
Daniel M. Alves <dmalves1991@gmail.com>
2021-02-09 15:50:46 -0300

    Update wikipedia.py

--- web scraping/wikipedia.py
+++ web scraping/wikipedia.py
@@ -1,20 +1,21 @@
-#!python3
-
 from bs4 import BeautifulSoup as bs
 import requests as rq
 
 while True:
-
     busca = input("Digite o tema: ")
-    busca = busca.replace(" ", '_')
-    url = "https://pt.wikipedia.org/wiki/"+busca
-    page = rq.get(url=url)
-    soup = bs(page.content, 'html.parser')
-    conteudo = soup.find(class_="mw-parser-output")
-    texto = conteudo.get_text()
+    if (busca == 'exit') or (busca == 'sair'):
+        break
+    else:
+        busca = busca.replace(" ", '_')
+        url = "https://pt.wikipedia.org/wiki/"+busca
+        page = rq.get(url=url)
+        soup = bs(page.content, 'html.parser')
 
-    print(texto)
-
-    with open(busca+'.txt', 'wb') as f:
-        f.write(texto.encode())
-    f.close()
+        with open(busca+'.txt', 'wb') as f:
+            print(f'\n:: {busca} ::\n')
+            for cont in soup.find_all('p'):
+                texto = cont.get_text()
+                texto = str(texto)
+                print(texto)
+                f.write(texto.encode())
+        f.close()
