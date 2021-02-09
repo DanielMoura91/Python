2dc6a56f21df64458f4615d25e7a651335daffe0
Daniel M. Alves <dmalves1991@gmail.com>
2021-02-09 20:53:56 -0300

    Update read_csv.py

--- análise de dados/read_csv.py
+++ análise de dados/read_csv.py
@@ -3,3 +3,8 @@ import pandas as pd
 df = pd.read_csv(r"data/ClientesBanco.csv",encoding='latin1')
 df = df.drop("CLIENTNUM",axis=1)
 display(df)
+
+df = df.dropna() #Exclui linha que tem itens vazios
+display(df.info()) #Mostra informações da tabela
+
+display(df.describe())
