f88cfc3e557fdd0c370121ab14d0bfc22480562c
Daniel M. Alves <dmalves1991@gmail.com>
2021-02-09 20:43:15 -0300

    Update read_csv.py

--- análise de dados/read_csv.py
+++ análise de dados/read_csv.py
@@ -1,8 +1,5 @@
 import pandas as pd
 
-#titanic = pd.read_csv("data/titanic.csv")
-
-titanic = pd.read_csv(
-    "https://gist.githubusercontent.com/michhar/2dfd2de0d4f8727f873422c5d959fff5/raw/fa71405126017e6a37bea592440b4bee94bf7b9e/titanic.csv")
-
-print(titanic.head())
+df = pd.read_csv(r"data/ClientesBanco.csv",encoding='latin1')
+df = df.drop("CLIENTNUM",axis=1)
+display(df)
