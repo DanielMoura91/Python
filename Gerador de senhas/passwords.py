b17ddcc58687e7f2897b5116ccf5ab0f62a0e173
Daniel M. Alves <dmalves1991@gmail.com>
2021-02-09 15:49:58 -0300

    Update passwords.py

--- Gerador de senhas/passwords.py
+++ Gerador de senhas/passwords.py
@@ -1,11 +1,10 @@
 import string
 import random
 
-
 def pass_generator(size=10, chars=string.ascii_uppercase + string.digits + string.ascii_lowercase):
     return ''.join(random.choice(chars) for _ in range(size))
 
+n = int(input('Quantas senhas seguras vc precisa? '))
 
-for i in range(10):
-    senha = pass_generator()
-    print(f'Senha {i+1}: ', senha)
+for i in range(n):  
+    print(f'Senha {i+1}: ', pass_generator())
