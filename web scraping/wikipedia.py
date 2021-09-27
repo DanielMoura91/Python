87dfcd0063879389bae4c08574110832c522849c
Daniel Moura Alves <dmalves1991@gmail.com>
2021-09-27 17:56:54 -0300

    Update wikipedia.py

--- web scraping/wikipedia.py
+++ web scraping/wikipedia.py
@@ -1,22 +1,32 @@
+import PySimpleGUIQt as sg
 from bs4 import BeautifulSoup as bs
 import requests as rq
 
+layout = [[sg.Text(" Digite o tema a ser pesquisado:")],
+          [sg.Input(key='Input')],
+          [sg.Output(key='Output')],
+          [sg.Button('Pesquisar', key='Search'), sg.Button('Limpar', key='Clear'), sg.Button('Sair', key='Quit')]]
+
+window = sg.Window('WikiSearch v1.0 - by Daniel Moura', layout)
+
 while True:
-    busca = input("Digite o tema: ")
-    if (busca == 'exit') or (busca == 'sair'):
+    event, values = window.read()
+
+    if event == sg.WINDOW_CLOSED or event == 'Quit':
         break
+    elif event == 'Clear':
+        window['Input'].update('')
+        window['Output'].update('')
     else:
+        busca = values['Input']
         busca = busca.replace(" ", '_')
         url = "https://pt.wikipedia.org/wiki/"+busca
-        page = rq.get(url=url)
-        soup = bs(page.content, 'html.parser')
-
-        with open(busca+'.txt', 'wb') as f:
-            busca = busca.replace("_", ' ')
-            print(f'\n:: {busca} ::\n')
-            for cont in soup.find_all('p'):
-                texto = cont.get_text()
-                texto = str(texto)
-                print(texto)
-                f.write(texto.encode())
-        f.close()
+        try:
+            page = rq.get(url=url)
+            soup = bs(page.content, 'html.parser')
+            cont = soup.find(id='mw-content-text')
+            texto = cont.get_text()
+            window['Output'].update(f'{texto}')
+        except:
+            window['Output'].update('Erro de Conexão! Tente outra hora')
+window.close()
